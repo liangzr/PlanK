e339965cafc3124c1667e6fca1e65125deefa631
liangzr <liangzr@outlook.com>
2016-11-23 16:00:37 +0800

    package init

---
+++ app/src/main/java/tech/liangzr/plank/ui/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package tech.liangzr.plank.ui.activity;
+
+import android.os.Bundle;
+
+import tech.liangzr.plank.R;
+import tech.liangzr.plank.base.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_home);
+    }
+}
